e590dc259e5527194335f3cd2f0dbdd85f3fea48
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2009-11-13 09:57:16 +0530

    Added the view args

--- openid_consumer/registration.py
+++ openid_consumer/registration.py
@@ -11,7 +11,7 @@ class RegistrationConsumer(AuthConsumer, DjangoOpenIDRegistrationConsumer):
     RegistrationForm   = RegistrationFormPasswordConfirm
 
     def user_is_unconfirmed(self, user):
-        return len(get_values(self.auth_db.view('auth_is_active/all', key=[])))
+        return len(get_values(self.auth_db.view('auth_is_active/all', key=[user.id, False])))
 
     def mark_user_confirmed(self, user):
         self.auth_db.delete(user)
