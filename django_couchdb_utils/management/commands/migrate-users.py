52e163803240511155bb31750224e892fed2b889
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-06-08 20:24:22 +0530

    Deleted files that were reorganized into separate apps

--- django_couchdb_utils/management/commands/migrate-users.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from optparse import make_option
-from django_couchdb_utils import auth
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        auth.migrate_users()
