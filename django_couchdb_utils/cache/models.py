e17b32b07a55db3e34cc5e612bff706c0b6c6e7d
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-06-11 12:37:08 +0530

    Fixed a SyntaxError in the cache models.

--- django_couchdb_utils/cache/models.py
+++ django_couchdb_utils/cache/models.py
@@ -11,8 +11,7 @@ class CacheRow(Document):
     @classmethod
     def get_row(cls, key):
         dbname = cls.get_db().dbname
-        r = cls.view('%s/cache_by_key' % dbname
-                     key=key, include_docs=True)
+        r = cls.view('%s/cache_by_key' % dbname, key=key, include_docs=True)
         return r.first() if r else None
 
     def _get_id(self):
