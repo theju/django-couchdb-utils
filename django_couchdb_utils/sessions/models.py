9125626c5a59a73934478d7a2c14cd3beaad93c6
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-06-14 19:36:23 +0530

    Fixing a ReferenceError in the classmethod.

--- django_couchdb_utils/sessions/models.py
+++ django_couchdb_utils/sessions/models.py
@@ -11,7 +11,7 @@ class Session(Document):
 
     @classmethod
     def get_session(cls, session_key):
-        r = cls.view('%s/sessions_by_key' % self._meta.app_label, key=session_key, include_docs=True)
+        r = cls.view('%s/sessions_by_key' % cls._meta.app_label, key=session_key, include_docs=True)
         try:
             return r.first()
         except ResourceNotFound:
