52243d8c59a0e4e61df6b07e3764d81bd06f2db5
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-06-14 20:25:30 +0530

    Fixing the openid_consumer form.

--- django_couchdb_utils/openid_consumer/forms.py
+++ django_couchdb_utils/openid_consumer/forms.py
@@ -2,19 +2,23 @@ from django import forms
 from django_couchdb_utils.auth.models import User
 from django_openid.forms import RegistrationForm as DjangoOpenidRegistrationForm, \
                                 RegistrationFormPasswordConfirm as DjangoOpenidRegistrationFormPasswordConfirm
+from couchdbkit.exceptions import ResourceNotFound
 
 class RegistrationForm(DjangoOpenidRegistrationForm):
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
-        self.auth_db = User.get_db()
 
     def save(self):
         user = User(**self.cleaned_data)
-        return user.store(self.auth_db)
+        return user.store()
 
     def clean_email(self):
         email = self.cleaned_data.get('email', '')
-        if self.no_duplicate_emails and len(self.auth_db.view('auth_email/all', key = email)) > 0:
+        try:
+            email_count = User.view('%s/users_by_email' % User._meta.app_label, key = email).count()
+        except ResourceNotFound:
+            email_count = 0
+        if self.no_duplicate_emails and email_count > 0:
             raise forms.ValidationError, self.duplicate_email_error
         return email
 
